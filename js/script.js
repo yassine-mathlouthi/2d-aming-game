6657311a99537eb9a97012d06f51cbbcb4ae84b2
yassine-mathlouthi <yassinemt08@gmail.com>
2024-10-31 13:42:15 +0100

    creating the target

--- js/script.js
+++ js/script.js
@@ -3,9 +3,24 @@ const timerElementNum = document.querySelector("#timer-Num")
 const startTimerElementNum = document.getElementById("start-timer");
 let startTimerCount = 4 ; 
 let startTimer ; 
-
 button.addEventListener("mousedown",startGame)
+// 2
+const container = document.querySelector(".container");
+var target = document.createElement("img")
+target.setAttribute("class","target") ; 
+target.setAttribute("src","./img/new-moon.png")
+const contHeight = container.offsetHeight;
+const contWidth = container.offsetWidth;
+setInterval(() => {
+  const randY = Math.random() * (contHeight - 100);
+  const randX = Math.random() * (contWidth - 100);
+  target.style.position = "absolute";
+  target.style.top = randY + "px";
+  target.style.left = randX + "px";
+}, 1000);
 
+
+// 2
 
 async function startGame(){
   startTimer=setInterval(()=>{
